18561b7b3b0dee21fd8e4fd66d9b27ee963f7dbb
Noh Suyeon <suy00065@gmail.com>
2022-10-11 13:26:09 +0900

    Spring IoC 실습 1

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/User.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/User.java
@@ -4,7 +4,7 @@ public class User {
     final String email;
     final String phoneNumber;
 
-    User(String email, String phoneNumber) {
+    public User(String email, String phoneNumber) {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
---
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/xml/XmlMain.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.edu.springframework.messagesender.xml;
+
+import com.nhnacademy.edu.springframework.messagesender.MessageSender;
+import com.nhnacademy.edu.springframework.messagesender.MessageSenderService;
+import com.nhnacademy.edu.springframework.messagesender.User;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XmlMain {
+    public static void main(String[] args) {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            MessageSender emailSender = context.getBean("EmailMessageSender", MessageSender.class);
+            MessageSender smsSender = context.getBean("SmsMessageSender", MessageSender.class);
+
+            User user = new User("random@naver.com", "010-0000-0000");
+            emailSender.sendMessage(user, "This is message");
+            System.out.println("------------------");
+            smsSender.sendMessage(user, "This is sms message");
+        }
+    }
+}
