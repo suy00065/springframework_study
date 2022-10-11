ffabfebe84595d08fb99bd1aa1164255f3520e7f
Noh Suyeon <suy00065@gmail.com>
2022-10-11 13:49:56 +0900

    Spring IoC 실습 - scope

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
 public class SmsMessageSender implements MessageSender {
+
+    public SmsMessageSender() {
+        System.out.println("SmsMessageSender initiated !");
+    }
     public void sendMessage(User user, String message) {
         System.out.println("SMS Message Sent to " + user.getPhoneNumber() + " : " + message);
     }
--- src/main/java/com/nhnacademy/edu/springframework/messagesender/xml/XmlMain.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/xml/XmlMain.java
@@ -8,13 +8,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class XmlMain {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-            MessageSender emailSender = context.getBean("EmailMessageSender", MessageSender.class);
-            MessageSender smsSender = context.getBean("SmsMessageSender", MessageSender.class);
+            /*MessageSender emailSender = context.getBean("EmailMessageSender", MessageSender.class);
+            MessageSender smsSender = context.getBean("SmsMessageSender", MessageSender.class);*/
 
             User user = new User("random@naver.com", "010-0000-0000");
-            emailSender.sendMessage(user, "This is message");
+/*            emailSender.sendMessage(user, "This is message");
             System.out.println("------------------");
-            smsSender.sendMessage(user, "This is sms message");
+            smsSender.sendMessage(user, "This is sms message");*/
+
+            System.out.println();
+            context.getBean("EmailMessageSender", MessageSender.class).sendMessage(user, "This is message");
+            System.out.println("----------------------------------");
+            context.getBean("EmailMessageSender", MessageSender.class).sendMessage(user, "This is message");
+            System.out.println("----------------------------------");
+
+            context.getBean("SmsMessageSender", MessageSender.class).sendMessage(user, "This is sms message");
+            System.out.println("----------------------------------");
+            context.getBean("SmsMessageSender", MessageSender.class).sendMessage(user, "This is sms message");
+            System.out.println("----------------------------------");
+
         }
     }
 }
