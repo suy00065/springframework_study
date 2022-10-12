01c974c6cf2a9e2736cc26db9af1b8972fce6af8
Noh Suyeon <suy00065@gmail.com>
2022-10-12 09:53:32 +0900

    @Autowired 배열로 의존성 주입까지

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
@@ -1,20 +1,36 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
-public class MessageSenderService {
-    private MessageSender messageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 
+import java.util.List;
+
+public class MessageSenderService {
+
+    private MessageSender messageSender;
+    private List<MessageSender> messageSenders;
+
+    //@Autowired
     public MessageSenderService(MessageSender messageSender) {
+        System.out.println("------ Constructor Injection ------");
         this.messageSender = messageSender;
     }
 
-    public MessageSenderService() {
-
+    @Autowired
+    public MessageSenderService(List<MessageSender> messageSenders) {
+        this.messageSenders = messageSenders;
     }
 
+    /*public MessageSenderService() {
+        System.out.println("---------- Default Constructor Injection ----------");
+    }*/
+
+    /*@Autowired  // setter injection
+    @Required
     public void setSmsMessageSender(MessageSender messageSender) {
         System.out.println("setMessageSender invoked!");
         this.messageSender = messageSender;
-    }
+    }*/
 
     public void setMessageSender(MessageSender messageSender) {
         System.out.println("set by setMessageSender");
@@ -22,6 +38,9 @@ public class MessageSenderService {
     }
 
     public void messageSend() {
-        messageSender.sendMessage(new User("random@naver.com", "010-0000-0000"), "test 메시지1");
+        //messageSender.sendMessage(new User("random@naver.com", "010-0000-0000"), "test 메시지1");
+
+        messageSenders.get(0).sendMessage(new User("random1@naver.com", "010-0000-0000"), "test 메시지1");
+        messageSenders.get(1).sendMessage(new User("random2@naver.com", "010-1000-0000"), "test 메시지2");
     }
 }
