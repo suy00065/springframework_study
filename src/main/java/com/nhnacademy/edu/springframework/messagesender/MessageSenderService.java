0ccbd19c3cd4f082613aca64fc571bab805d6fba
Noh Suyeon <suy00065@gmail.com>
2022-10-12 10:53:08 +0900

    Spring DI 실습(annotation) - MessageSendService

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
+import com.nhnacademy.edu.springframework.messagesender.annotation.SMS;
+import com.nhnacademy.edu.springframework.messagesender.annotation.SMS2;
+import com.nhnacademy.edu.springframework.messagesender.annotation.SelectSendType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 
 import java.util.List;
@@ -11,15 +15,28 @@ public class MessageSenderService {
     private List<MessageSender> messageSenders;
 
     //@Autowired
-    public MessageSenderService(MessageSender messageSender) {
+    /*public MessageSenderService(MessageSender messageSender) {
         System.out.println("------ Constructor Injection ------");
         this.messageSender = messageSender;
-    }
+    }*/
 
-    @Autowired
+    //@Autowired(required = true)
     public MessageSenderService(List<MessageSender> messageSenders) {
         this.messageSenders = messageSenders;
     }
+
+    @Autowired
+    public MessageSenderService(@SMS2("smsMessageSender") MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    /*public MessageSenderService(@SMS(selectSendType = SelectSendType.SMS_) MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }*/
+
+    /*public MessageSenderService(@Qualifier("smsMessageSender") MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }*/
 
     /*public MessageSenderService() {
         System.out.println("---------- Default Constructor Injection ----------");
@@ -38,9 +55,9 @@ public class MessageSenderService {
     }
 
     public void messageSend() {
-        //messageSender.sendMessage(new User("random@naver.com", "010-0000-0000"), "test 메시지1");
+        messageSender.sendMessage(new User("random@naver.com", "010-0000-0000"), "test 메시지1");
 
-        messageSenders.get(0).sendMessage(new User("random1@naver.com", "010-0000-0000"), "test 메시지1");
-        messageSenders.get(1).sendMessage(new User("random2@naver.com", "010-1000-0000"), "test 메시지2");
+        //messageSenders.get(0).sendMessage(new User("random1@naver.com", "010-0000-0000"), "test 메시지1");
+        //messageSenders.get(1).sendMessage(new User("random2@naver.com", "010-1000-0000"), "test 메시지2");
     }
 }
